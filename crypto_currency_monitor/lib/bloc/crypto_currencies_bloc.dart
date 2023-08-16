8268388cc1f63959a5711badfaac979f1fe0dccc
Damien Kake <damien.kake@youscribe.com>
2023-08-16 10:35:59 +0200

    localization

--- crypto_currency_monitor/lib/bloc/crypto_currencies_bloc.dart
+++ crypto_currency_monitor/lib/bloc/crypto_currencies_bloc.dart
@@ -5,10 +5,6 @@ import 'package:crypto_currency_monitor/bloc/shared/bloc.dart';
 
 class CryptoCurrenciesBloc implements BaseBloc {
 
-  CryptoCurrenciesBloc() {
-
-  }
-
   @override
   void dispose() {
 
