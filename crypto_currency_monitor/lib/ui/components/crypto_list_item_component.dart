79de60a69dc7e8c38a5f19c8b08dafb9df2871d6
Damien Kake <damien.kake@youscribe.com>
2023-08-31 15:14:55 +0200

    added ripple effect on listview items

--- crypto_currency_monitor/lib/ui/components/crypto_list_item_component.dart
+++ crypto_currency_monitor/lib/ui/components/crypto_list_item_component.dart
@@ -13,79 +13,81 @@ class CryptoListItemComponent extends StatelessWidget {
   final BuildContext context;
   int get index => rank - 1;
 
-  CryptoListItemComponent(
-      {required this.cryptoCurrency,
+  const CryptoListItemComponent(
+      {super.key, required this.cryptoCurrency,
       required this.bloc,
       required this.rank,
       required this.context});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: SizedBox(
-        height: 50,
-        child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          Container(
-              alignment: Alignment.center,
-              child: StreamBuilder(
-                  stream: bloc.favoriteStream,
-                  builder: (context, snapshot) {
-                    var icon = snapshot.hasData &&
-                            (snapshot.data?.$1 != null &&
-                                snapshot.data?.$2 != null) &&
-                            (snapshot.data!.$1 == index && snapshot.data!.$2)
-                        ? Icons.favorite
-                        : Icons.favorite_border;
-
-                    return IconButton(
-                        onPressed: () => bloc.cryptoCurrencyFavoriteTapped(
-                            cryptoCurrency, index),
-                        icon: Icon(icon,
-                            size: 20, color: AppColors.likeButtonColor));
-                  })),
-          SizedBox(
-            width: 30,
-            child: Text(rank.toString(),
-                textAlign: TextAlign.start,
-                style: AppStyles.cryptoListHeaderTextStyle()),
-          ),
-          Expanded(
-              child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(right: 3),
-                height: 35,
-                child: RoundedNetworkImage(
-                  imageUrl: cryptoCurrency.image!,
-                  borderRadius: const BorderRadius.all(Radius.circular(30)),
+    return Material(
+      child: InkWell(
+        child: SizedBox(
+          height: 50,
+          child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Container(
+                alignment: Alignment.center,
+                child: StreamBuilder(
+                    stream: bloc.favoriteStream,
+                    builder: (context, snapshot) {
+                      var icon = snapshot.hasData &&
+                              (snapshot.data?.$1 != null &&
+                                  snapshot.data?.$2 != null) &&
+                              (snapshot.data!.$1 == index && snapshot.data!.$2)
+                          ? Icons.favorite
+                          : Icons.favorite_border;
+    
+                      return IconButton(
+                          onPressed: () => bloc.cryptoCurrencyFavoriteTapped(
+                              cryptoCurrency, index),
+                          icon: Icon(icon,
+                              size: 20, color: AppColors.likeButtonColor));
+                    })),
+            SizedBox(
+              width: 30,
+              child: Text(rank.toString(),
+                  textAlign: TextAlign.start,
+                  style: AppStyles.cryptoListHeaderTextStyle()),
+            ),
+            Expanded(
+                child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(right: 3),
+                  height: 35,
+                  child: RoundedNetworkImage(
+                    imageUrl: cryptoCurrency.image!,
+                    borderRadius: const BorderRadius.all(Radius.circular(30)),
+                  ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(cryptoCurrency.name!,
-                      style: AppStyles.normalTextStyle()),
-                  Text(cryptoCurrency.symbol!,
-                      style: AppStyles.secondaryTextStyle())
-                ],
-              )
-            ],
-          )),
-          Expanded(
-              child: Center(
-                  child: Text(
-                      AppStyles.formatNumber(cryptoCurrency.currentPrice),
-                      style: AppStyles.normalTextStyle()))),
-          Expanded(
-              child: Center(
-                  child: Text(AppStyles.formatNumber(cryptoCurrency.marketCap),
-                      style: AppStyles.normalTextStyle())))
-        ]),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(cryptoCurrency.name!,
+                        style: AppStyles.normalTextStyle()),
+                    Text(cryptoCurrency.symbol!,
+                        style: AppStyles.secondaryTextStyle())
+                  ],
+                )
+              ],
+            )),
+            Expanded(
+                child: Center(
+                    child: Text(
+                        AppStyles.formatNumber(cryptoCurrency.currentPrice),
+                        style: AppStyles.normalTextStyle()))),
+            Expanded(
+                child: Center(
+                    child: Text(AppStyles.formatNumber(cryptoCurrency.marketCap),
+                        style: AppStyles.normalTextStyle())))
+          ]),
+        ),
+        onTap: () => bloc.cryptoCurrencySelected(cryptoCurrency, index),
       ),
-      onTap: () => bloc.cryptoCurrencySelected(cryptoCurrency, index),
     );
   }
 }
